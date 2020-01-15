c62cb30421bac6fa800462035c19bac99ca71cf3
ndallapozza <nicoladallapozza@gmail.com>
2020-01-15 14:53:00 +0100

    File creation following https://medium.com/@apoddar573/making-your-own-custom-environment-in-gym-c3b65ff8cdaa and https://github.com/MattChanTK/gym-maze/tree/master/gym_maze

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="gym_quantum_maze_learning",
+      version="0.1",
+      url="https://github.com/Buffoni/quantum_maze_learning",
+      author="Lorenzo Buffoni, Nicola Dalla Pozza",
+      install_requires=['gym']#And any other dependencies required
+)
